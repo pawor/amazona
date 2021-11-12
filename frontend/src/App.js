0fa046962b6be95188c1d080a5bb381c033764e0
pawor <pawor.emi@gmail.com>
2021-11-12 20:10:40 +0700

    create backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,28 @@
 import React from 'react';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
     return (
-        <div class="grid-container">
-            <header class="row">
-                <div>
-                    <a class="brand" href="/">amazona</a>
-                </div>
-                <div>
-                    <a href="/cart">Cart</a>
-                    <a href="/signin">Sign In</a>
-                </div>
-            </header>
-            <main>
-                <div>
-                    <div className="row center">
-                        {data.products.map(product => (
-                            <div key={product._id} className="card">
-                                <a href={`/product/${product._id}`}>
-                                    <img className="medium" src={product.image} alt={product.name} />
-                                </a>
-                                <div className="card-body">
-                                    <a href={`/product/${product._id}`}>
-                                        <h2>{product.name}</h2>
-                                    </a>
-                                    <div className="rating">
-                                        <span><i className="fa fa-star"></i></span>
-                                        <span><i className="fa fa-star"></i></span>
-                                        <span><i className="fa fa-star"></i></span>
-                                        <span><i className="fa fa-star"></i></span>
-                                        <span><i className="fa fa-star"></i></span>
-                                    </div>
-                                    <div className="price">${product.price}</div>
-                                </div>
-                            </div>
-                        ))
-                        }
+        <BrowserRouter>
+            <div class="grid-container">
+                <header class="row">
+                    <div>
+                        <a class="brand" href="/">amazona</a>
                     </div>
-                </div>
-            </main>
-            <footer class="row center">All right reserved</footer>
-        </div>
+                    <div>
+                        <a href="/cart">Cart</a>
+                        <a href="/signin">Sign In</a>
+                    </div>
+                </header>
+                <main>
+                    <Route path="/product/:id" component={ProductScreen}></Route>
+                    <Route path="/" component={HomeScreen} exact></Route>
+                </main>
+                <footer class="row center">All right reserved</footer>
+            </div>
+        </BrowserRouter>
     );
 }
 
