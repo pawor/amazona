b4d043938fb6bec645476b780bf6604fb884738f
pawor <pawor.emi@gmail.com>
2021-11-13 17:03:55 +0700

    create CartUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 
 function App() {
+    const cart = useSelector((state) => state.cart);
+    const { cartItems } = cart;
+
     return (
         <BrowserRouter>
             <div class="grid-container">
                 <header class="row">
                     <div>
-                        <a class="brand" href="/">amazona</a>
+                        <Link class="brand" to="/">amazona</Link>
                     </div>
                     <div>
-                        <a href="/cart">Cart</a>
-                        <a href="/signin">Sign In</a>
+                        <Link to="/cart">Cart
+                        {cartItems.length > 0 && (
+                            <span className="badge">{cartItems.length}</span>
+                        )}
+                        </Link>
+                        <Link to="/signin">Sign In</Link>
                     </div>
                 </header>
                 <main>
