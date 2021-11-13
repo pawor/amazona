1a44d64860f67fdff7940d201e0dc15a32e014b8
pawor <pawor.emi@gmail.com>
2021-11-13 17:45:13 +0700

    edit CartUI

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -17,6 +17,14 @@ export default function CartScreen(props) {
             dispatch(addToCart(productId, qty));
         }
     },[dispatch,productId, qty]);
+    const removeFromCartHandler = (id) => {
+        //delete action
+    };
+
+    const checkoutHandler = (props) => {
+       props.history.push('/signin?redirect=shipping');
+    };
+
     return (
         <div className="row top">
             <div className="col-2">
@@ -36,12 +44,56 @@ export default function CartScreen(props) {
                                         <div className="min-30">
                                             <Link to={`/product/${item.product}`}>{item.name}</Link>
                                         </div>
+                                        <div>
+                                            <select 
+                                                value={item.qty} 
+                                                onChange={(e) => 
+                                                    dispatch(
+                                                        addToCart(item.product,Number(e.target.value))
+                                                    )}>
+                                                        {[...Array(item.countInStock).keys()].map((x) => (
+                                                        <option key={x+1} value={x+1}>
+                                                            {x + 1}
+                                                        </option>
+                                                    ))}
+                                            </select>
+                                        </div>
+                                        <div>
+                                            ${item.price}
+                                        </div>
+                                        <div>
+                                            <button type="button" onClick={() => removeFromCartHandler(item.product)}>
+                                                Delete
+                                            </button>
+                                        </div>
                                     </div>
                                 </li>
                             ))
                         }
                     </ul>
                 )}
+            </div>
+            <div className="col-1">
+                <div className="card card-body">
+                    <ul>
+                        <li>
+                            <h2>
+                                Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : $
+                                {cartItems.reduce((a,c) => a + c.price * c.qty, 0)}
+                            </h2>
+                        </li>
+                        <li>
+                            <button 
+                                type="button" 
+                                onClick={checkoutHandler} 
+                                className="primary block" 
+                                disabled={cartItems.length === 0}
+                            >
+                                Proceed to Checkout
+                            </button>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
